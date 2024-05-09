172c7b14611a15e58601ac5ff6ab6d4e1c000bdd
Raze <ze.wang@diact.com>
2024-05-09 15:12:55 +0800

    chore: 添加别名配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+/*
+ * @Author: ze.wang@diact.com
+ * @Date: 2024-05-09 14:18:03
+ * @Function: Please Input Function
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: [
+      {
+        find: '/@',
+        replacement: path.resolve(__dirname, './src'),
+      },
+    ],
+  },
 })
